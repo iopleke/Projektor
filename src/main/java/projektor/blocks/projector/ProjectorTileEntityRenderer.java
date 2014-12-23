8e24ea34f2740d7bac7ce91834e7a09af0c49aeb
jakimfett <jakimfett@picraft.com>
2014-12-22 19:49:33 -0800

    Uploading for @AfterLifeLochie flat out copied WorldProxy and BlockContainerProxy, not my code, I make no claim to it, will be replaced later

--- src/main/java/projektor/blocks/projector/ProjectorTileEntityRenderer.java
+++ src/main/java/projektor/blocks/projector/ProjectorTileEntityRenderer.java
@@ -1,30 +1,32 @@
 package projektor.blocks.projector;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import projektor.proxy.BlockContainerProxy;
+import projektor.proxy.WorldProxy;
 
 public class ProjectorTileEntityRenderer extends TileEntitySpecialRenderer
 {
-	ProjectorModel model;
-	private RenderBlocks renderBlocks;
-	private World world;
+    ProjectorModel model;
+    private RenderBlocks renderBlocks;
+    private World world;
 
-	public ProjectorTileEntityRenderer()
-	{
-		this.model = new ProjectorModel();
-		this.world = null;
-		this.renderBlocks = new RenderBlocks();
-	}
+    public ProjectorTileEntityRenderer()
+    {
+        this.model = new ProjectorModel();
+        this.world = null;
+        this.renderBlocks = new RenderBlocks();
+    }
 
-	@Override
-	public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float scale)
-	{
-		if (tileEntity instanceof ProjectorTileEntity)
-		{
+    @Override
+    public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float scale)
+    {
+        if (tileEntity instanceof ProjectorTileEntity)
+        {
 
 //			ProjectorTileEntity blueprintProjector = (ProjectorTileEntity) tileEntity;
 //			int facing = blueprintProjector.getFacing();
@@ -41,11 +43,30 @@ public class ProjectorTileEntityRenderer extends TileEntitySpecialRenderer
 //			}
 //			model.render(0.0625F);
 //			GL11.glPopMatrix();
-			this.world = tileEntity.getWorldObj();
-			this.renderBlocks = new RenderBlocks((IBlockAccess) world);
-			System.out.println("Rendering block at: x" + tileEntity.xCoord + ", y" + tileEntity.yCoord + ", z" + tileEntity.zCoord);
-			renderBlocks.renderBlockByRenderType(Blocks.stone, tileEntity.xCoord, (int) tileEntity.yCoord, (int) tileEntity.zCoord);
-		}
-	}
+//            this.world = tileEntity.getWorldObj();
+//            this.renderBlocks = new RenderBlocks((IBlockAccess) world);
+            System.out.println("Rendering block at: x" + tileEntity.xCoord + ", y" + tileEntity.yCoord + ", z" + tileEntity.zCoord);
+//            renderBlocks.renderBlockByRenderType(Blocks.stone, tileEntity.xCoord, (int) tileEntity.yCoord, (int) tileEntity.zCoord);
+
+            Block skinBlock = Blocks.dirt;
+            int skinBlockMetadata = 0;
+            RenderBlocks proxyRender = new RenderBlocks(new WorldProxy(world, skinBlockMetadata, 7));
+            proxyRender.setRenderBoundsFromBlock(skinBlock);
+            if (skinBlockMetadata != 0)
+            {
+                BlockContainerProxy proxyContainer;
+                for (int i = 0; i < 6; i++)
+                {
+                    proxyRender.setOverrideBlockTexture(skinBlock.getIcon(i, skinBlockMetadata));
+                    proxyContainer = new BlockContainerProxy(skinBlock, i);
+                    proxyRender.renderStandardBlock(proxyContainer, (int) x, (int) y, (int) z);
+                }
+            } else
+            {
+                proxyRender.renderStandardBlock(skinBlock, (int) x, (int) y, (int) z);
+            }
+            proxyRender.clearOverrideBlockTexture();
+        }
+    }
 
 }
---
+++ src/main/java/projektor/proxy/BlockContainerProxy.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projektor.proxy;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
+
+/**
+ * Block container access proxy. Allows the spoofing of containers and their associated special block rendering.
+ *
+ * @author AfterLifeLochie
+ *
+ */
+public class BlockContainerProxy extends Block
+{
+    private Block myBlock;
+    private int mySide;
+
+    /**
+     * Default constructor
+     *
+     * @param block The spoofed block
+     * @param side The spoofed side
+     */
+    public BlockContainerProxy(Block block, int side)
+    {
+        super(block.getMaterial());
+        myBlock = block;
+        mySide = side;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public boolean shouldSideBeRendered(IBlockAccess p_149646_1_, int p_149646_2_, int p_149646_3_, int p_149646_4_,
+            int side)
+    {
+        return side == mySide ? super.shouldSideBeRendered(p_149646_1_, p_149646_2_, p_149646_3_, p_149646_4_, side)
+                : false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIcon(IBlockAccess p_149673_1_, int p_149673_2_, int p_149673_3_, int p_149673_4_, int p_149673_5_)
+    {
+        return myBlock.getIcon(p_149673_1_, p_149673_2_, p_149673_3_, p_149673_4_, p_149673_5_);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIcon(int p_149691_1_, int p_149691_2_)
+    {
+        return myBlock.getIcon(p_149691_1_, p_149691_2_);
+    }
+}
