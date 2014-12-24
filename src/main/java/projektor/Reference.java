ecc86cfe034391d2094b55ff44a23dcc38ecc4b6
jakimfett <jakimfett@picraft.com>
2014-12-24 12:10:56 -0800

    Add color list

--- src/main/java/projektor/Reference.java
+++ src/main/java/projektor/Reference.java
@@ -37,6 +37,8 @@ public class Reference
 
         // BASE IDs
         public static final String PROJECTOR = "projector";
+
+        public static final String SCHEMATIC_DEFAULT = "schematic";
     }
 
     public static final class Versioning
@@ -62,6 +64,28 @@ public class Reference
     {
         private static final String PREFIX = Reference.Naming.ID + ":";
         public static final String DUMMY = PREFIX + "dummyBlockIcon";
+    }
+
+    public static final class Colors
+    {
+
+        public static final String Black = "\u00A70";
+        public static final String DarkBlue = "\u00A71";
+        public static final String DarkGreen = "\u00A72";
+        public static final String DarkCyan = "\u00A73";
+        public static final String DarkRed = "\u00A74";
+        public static final String Purple = "\u00A75";
+        public static final String Orange = "\u00A76";
+        public static final String LightGrey = "\u00A77";
+        public static final String DarkGrey = "\u00A78";
+        public static final String LightBlue = "\u00A79";
+        public static final String LightGreen = "\u00A7a";
+        public static final String LightCyan = "\u00A7b";
+        public static final String LightRed = "\u00A7c";
+        public static final String Pink = "\u00A7d";
+        public static final String Yellow = "\u00A7e";
+        public static final String White = "\u00A7f";
+
     }
 
 }
